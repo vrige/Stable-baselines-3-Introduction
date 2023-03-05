402b2e21aff7007ea6d61df5a4297df02e928452
Manuel <manuel.96524@gmail.com>
2023-03-05 18:01:46 +0100

    First commit - first tutorial video

---
+++ intro1.py
@@ -0,0 +1,33 @@
+import gym
+from stable_baselines3 import A2C
+env = gym.make("LunarLander-v2")
+
+env.reset()
+
+# some info you can get
+print("a sample of actions: ", env.action_space.sample())
+print("observation space shape: ", env.observation_space.shape)
+print("a sample of observation space: ", env.observation_space.sample())
+
+# # this is an example of the model working on 200 steps and printing all the rewards
+# for step in range(200):
+#     env.render()
+#     obs, reward, done, info = env.step(env.action_space.sample()) # perform a random action
+#     print(reward)
+
+# let's try to use the RL algorithm A2C
+model = A2C("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=10000)
+
+episodes = 10
+
+for ep in range(episodes):
+    obs = env.reset()
+    done = False
+    while not done:
+        env.render()
+        obs, reward, done, info = env.step(env.action_space.sample())  # perform a random action
+
+# notice that it's very easy to change the algorithm. Just import the one that you like and change the line with the
+# model definition
+env.close()
